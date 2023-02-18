ced5916cde41cdb192c5c0f3e0640c078830d0bf
Yilma Kidanie <yilma.k2015@gmail.com>
2023-02-18 00:13:09 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -prints alphabet
+ * Return: Always 0
+ */
+int main(void)
+{
+	char C;
+
+
+	for (C = 'a'; C <= 'z'; c++);
+	putchar(C);
+	putchar('\n');
+	return (0);
+}
+
