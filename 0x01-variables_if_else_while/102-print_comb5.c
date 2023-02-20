08fad63d46e2ee5f3e6390deb7ded17a4864dedd
Yilma Kidanie <yilma.k2015@gmail.com>
2023-02-19 19:56:01 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	int m, n;
 
-	for (m = '0'; m <= '98'; m++)
+	for (m = '0'; m <= 98; m++)
 	{
 
-	for (n = m + 1; n <= '99'; n++)
+	for (n = m + 1; n <= 99; n++)
 	{
 	putchar((m / 10) + '0');
 	putchar((m % 10) + '0');
 	putchar(' ');
 	putchar((n / 10) + '0');
 	putchar((n % 10) + '0');
-	if (m == '98' && n == '99')
+	if (m == 98 && n == 99)
 	continue;
 	putchar(',');
 	putchar(' ');
