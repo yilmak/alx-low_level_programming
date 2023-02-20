a444106d986e71ad8ff5b7c8bde70ffa92f01a0d
Yilma Kidanie <yilma.k2015@gmail.com>
2023-02-19 19:42:38 -0800

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers.
+ * Return: Always 0
+ */
+int main(void)
+{
+	int m, n;
+
+	for (m = '0'; m <= '98'; m++)
+	{
+
+	for (n = m + 1; n <= '99'; n++)
+	{
+	putchar((m / 10) + '0');
+	putchar((m % 10) + '0');
+	putchar(' ');
+	putchar((n / 10) + '0');
+	putchar((n % 10) + '0');
+	if (m == '98' && n == '99')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+
