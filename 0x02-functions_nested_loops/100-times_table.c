5b0dd7d2741e2f62ef730cb969e01d1fb8bb4182
Yilma Kidanie <yilma.k2015@gmail.com>
2023-02-21 13:28:57 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,7 @@ void print_times_table(int n)
 	for (mult = 1; mult <= n; mult++)
 	{
 	_putchar(',');
-	_putchar(',');
+	_putchar(' ');
 	prod = num * mult;
 	if (prod <= 99)
 	_putchar(' ');
@@ -29,7 +29,7 @@ void print_times_table(int n)
 	if (prod >= 100)
 	{
 	_putchar((prod / 100) + '0');
-	_putchar((prod / 10) % 10 + '0');
+	_putchar(((prod / 10)) % 10 + '0');
 	}
 	else if (prod <= 99 && prod >= 10)
 	{
