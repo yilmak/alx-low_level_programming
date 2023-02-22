c437b272478ecbfe0ece9d5d3f221a2ea6bbb5cc
Yilma Kidanie <yilma.k2015@gmail.com>
2023-02-22 09:19:27 -0800

    1. Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - cuases an infinite loop
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i = 10)
+	 * {
+	 * putchhar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
