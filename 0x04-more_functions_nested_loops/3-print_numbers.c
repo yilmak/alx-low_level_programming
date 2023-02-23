8fed301f4ddea45ddf28d7f87d5ff80747037fcc
Yilma Kidanie <yilma.k2015@gmail.com>
2023-02-23 10:51:04 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - Prints the numbers from 0 to 9
+ * Return:
+ */
+
+void print_numbers(void)
+
+{
+	int n;
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
