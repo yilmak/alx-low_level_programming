b3241d2a92b7a138a0895f93895b3063ad6ca143
Yilma Kidanie <yilma.k2015@gmail.com>
2023-02-23 11:37:50 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times of numbers from 0 to 14
+ * Return: 10 times of the numbers 0 to 14
+ */
+
+void more_numbers(void)
+
+{
+
+	int m, n;
+
+	for (m = 0; m <= 10; m++)
+	{
+	for (n = 0; n <= 14; n++)
+	{
+	if (n > 9)
+	{
+	_putchar((n / 10) + '0');
+	}
+	_putchar((n % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
