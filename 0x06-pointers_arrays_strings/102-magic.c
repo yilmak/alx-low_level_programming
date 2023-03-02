33d3b16f5c3bb72b39770c6ef3c020123abac6be
Yilma Kidanie <yilma.k2015@gmail.com>
2023-03-02 08:09:34 -0800

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: nothing
+ */
+
+int main(void)
+{
+	int n;
+	int a[s];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+
+/**
+ * main - write your line of code here
+ * @a: parameter
+ * %d\n: new
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
+ */
+
+
+/* ... so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
