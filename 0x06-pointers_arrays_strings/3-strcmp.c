1b1a60b2e2b8400f428bf05d07fa035a413063ec
Yilma Kidanie <yilma.k2015@gmail.com>
2023-03-02 03:32:58 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,15 +4,15 @@
  * _strcmp - Compares pointers to two strings
  * @s1: A pointer to the first string to be compared
  * @s2: A pointer to the second string to be compared
- * @n: The maximum number of bytes to be copied from src
- * Return: If srt1 < str2, the negative difference of the first unmatched characters
+ * Return: If srt1 < str2, the negative difference
  * If str1 == str2, 0
- * If str1 > str2, the positive difference of the first unmatched charchters.
+ * If str1 > str2, the positive difference.
  */
 
 int _strcmp(char *s1, char *s2)
 
 {
+
 	while (*s1 && *s2 && *s1 == *s2)
 	{
 	s1++;
